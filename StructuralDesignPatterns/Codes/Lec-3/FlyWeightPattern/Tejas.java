ef1212fb75dec4b29f127ac4a483e05ed45dba9c
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-02-26 10:41:48 +0530

    StructuralDesignPatternsLec-3

--- StructuralDesignPatterns/Codes/Lec-3/FlyWeightPattern/Tejas.java
+++ StructuralDesignPatterns/Codes/Lec-3/FlyWeightPattern/Tejas.java
@@ -1,12 +1,12 @@
-public class Tejas implements IAircraft{
+public class Tejas implements IAircraft {
 
-    //intrinsic property
+    // intrinsic property
     private final String name = "Tejas";
     private final int seater = 2;
     private final String dimensions = "15m long 4 m wide";
     private final String wingSpan = "40 feet";
 
-    //extrinsic property
+    // extrinsic property
     public double getTotalTimeToReachDestination(int srcX, int srcY, int desX, int desY, int speed) {
         return 5;
     }
