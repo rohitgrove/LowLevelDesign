7c61117c52c37f429d324a82a05361ad62a60ede
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-03-12 14:16:01 +0530

    Design-4 Meeting Sheduler

--- DesignProblem/Design-4 Meeting Sheduler/Codes/models/User.java
+++ DesignProblem/Design-4 Meeting Sheduler/Codes/models/User.java
@@ -2,7 +2,6 @@ package models;
 
 public class User {
     private String name;
-    //TODO: write down getter and setter for all the private elements
     private String email;
     private int id;
     private Calendar userCalendar;
@@ -11,4 +10,36 @@ public class User {
     public void respondToInvitation(Notification message) {}
     public void respondToMeeting() {}
     public void checkCalendar() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setCaleder(Calendar userCalendar) {
+        this.userCalendar = userCalendar;
+    }
+
+    public Calendar getCalendar() {
+        return this.userCalendar;
+    }
 }
