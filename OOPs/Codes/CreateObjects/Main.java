00cada604ab3745b3b8ed045c0176d9a06be300d
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-17 17:46:02 +0530

    CreateObjects

--- OOPs/Codes/CreateObjects/Main.java
+++ OOPs/Codes/CreateObjects/Main.java
@@ -15,19 +15,8 @@ public class Main {
         // A.study();
         // A.sleep();
 
-        // Parameterised ctor
+        // Parameteresed Ctor
         Student A = new Student(1, 12, "Rahul", 3);
-        // System.out.println(A.name);
-        // System.out.println(A.age);
-        // System.out.println(A.id);
-        // System.out.println(A.nos);
-
-        // A.bunk();
-        // A.study();
-        // A.sleep();
-
-        // Copy ctor
-        Student B = new Student(A);
         System.out.println(A.name);
         System.out.println(A.age);
         System.out.println(A.id);
@@ -36,5 +25,18 @@ public class Main {
         A.bunk();
         A.study();
         A.sleep();
+
+        Student B = new Student(1, 12, "Rena", 3);
+        Student C = new Student(1, 12, "Ranu", 3);
+        Student D = new Student(1, 12, "lokesh", 3);
+
+        // Copy ctor
+        // Student B = new Student(A);
+        // System.out.println(B.name);
+        // System.out.println(B.age);
+        // System.out.println(B.id);
+        // System.out.println(B.nos);
+
+        // B.study();
     }
 }
